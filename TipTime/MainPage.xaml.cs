10dca0a01fd2cb0b3ad59654d09704244e0882a3
MATHEUS VIEIRA <matheus.vieira137@etec.sp.gov.br>
2024-04-10 20:28:22 -0300

    update tiptime

--- TipTime/MainPage.xaml.cs
+++ TipTime/MainPage.xaml.cs
@@ -6,6 +6,55 @@
         {
             InitializeComponent();
         }
+
+        private void Percentage15Button_Clicked(object sender, EventArgs e)
+        {
+            TipPercentageSlider.Value = 15;
+        }
+
+        private void Percentage30Button_Clicked(object sender, EventArgs e)
+        {
+            TipPercentageSlider.Value = 30;
+        }
+
+        private void RoundDownButton_Clicked(object sender, EventArgs e)
+        {
+            //Arredondar o número para o inteiro menor ao valor.
+            //Pegar o valor da refeição
+            //Pegar a porcentagem que o usuário selecionou 
+            //Calcular o valor da gorjeta
+            //Arredondar
+            //Calcular o total
+            //Exibir 
+
+            double valorDaRefeição = Convert.ToDouble(ValueEntry.Text);
+            double porcentagemDaGorjeta = TipPercentageSlider.Value / 100;
+            double valorDaGorjeta = valorDaRefeição * porcentagemDaGorjeta;
+            valorDaGorjeta = Math.Floor(valorDaGorjeta);
+            double totalRefeição = valorDaGorjeta + valorDaRefeição;
+
+            TotalLabel.Text = totalRefeição.ToString();
+            TipLabel.Text = valorDaGorjeta.ToString();
+
+        }
+
+        private void RoundUpButton_Clicked(object sender, EventArgs e)
+        {
+            double valorDaRefeição = Convert.ToDouble(ValueEntry.Text);
+            double porcentagemDaGorjeta = TipPercentageSlider.Value / 100;
+            double valorDaGorjeta = valorDaRefeição * porcentagemDaGorjeta;
+            valorDaGorjeta = Math.Ceiling(valorDaGorjeta);
+            double totalRefeição = valorDaGorjeta + valorDaRefeição;
+
+            TotalLabel.Text = totalRefeição.ToString();
+            TipLabel.Text = valorDaGorjeta.ToString();
+        }
+
+        private void OnTipPercentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double valorDoSlider = TipPercentageSlider.Value;
+            TipPorcentagemLabel.Text = valorDoSlider.ToString();
+        }
     }
 
 }
